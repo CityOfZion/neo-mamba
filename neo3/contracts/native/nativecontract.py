c56111264b5e90614cbc4bb4957d5bc7e0fae4bf
Erik van den Brink <git@erikvandenbrink.com>
2021-01-13 10:52:13 +0100

    https://github.com/neo-project/neo/commit/7032907cbbebd9189bcd01e6b83328b864a0a5b2

--- neo3/contracts/native/nativecontract.py
+++ neo3/contracts/native/nativecontract.py
@@ -1193,14 +1193,17 @@ class NeoToken(Nep5Token):
         super(NeoToken, self).on_persist(engine)
 
         # distribute GAS for committee
+        index = engine.snapshot.persisting_block.index % len(settings.standby_committee)
         gas_per_block = self.get_gas_per_block(engine.snapshot)
         committee = self._get_committee_members(engine.snapshot)
-        pubkey = committee[engine.snapshot.persisting_block.index % len(settings.standby_committee)]
+        committee.sort()
+        pubkey = committee[index]
         account = to_script_hash(contracts.Contract.create_signature_redeemscript(pubkey))
         GasToken().mint(engine, account, gas_per_block * self._COMMITTEE_REWARD_RATIO / 100)
 
         # set next validators
         validators = committee[:settings.network.validators_count]
+        validators.sort()
         if self._validators_state is None:
             self._validators_state = _ValidatorsState(engine.snapshot, validators)
         self._validators_state.update(engine.snapshot, validators)
