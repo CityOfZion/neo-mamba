23e12502d824ecf7aaf59b79f60f57c861a775b2
Erik van den Brink <git@erikvandenbrink.com>
2021-01-19 11:28:57 +0100

    https://github.com/neo-project/neo/pull/1990

--- neo3/contracts/interop/contract.py
+++ neo3/contracts/interop/contract.py
@@ -46,10 +46,7 @@ def contract_update(engine: contracts.ApplicationEngine, script: bytes, manifest
     script_len = len(script)
     manifest_len = len(manifest)
 
-    # TODO: In preview 4 revert back to
-    # engine.add_gas(engine.STORAGE_PRICE * (script_len + manifest_len))
-    # They made a mistake in their storage price calculation logic where manifest size is never taken into account
-    engine.add_gas(engine.STORAGE_PRICE * script_len)
+    engine.add_gas(engine.STORAGE_PRICE * (script_len + manifest_len))
 
     contract = engine.snapshot.contracts.try_get(engine.current_scripthash, read_only=True)
     if contract is None:
