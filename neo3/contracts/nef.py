2185fc93c6342624c7c9bcec826345f3badf9588
Erik van den Brink <git@erikvandenbrink.com>
2021-02-03 13:57:33 +0100

    https://github.com/neo-project/neo/pull/2185

--- neo3/contracts/nef.py
+++ neo3/contracts/nef.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 import hashlib
+from typing import List
 from neo3.core import serialization, types, Size as s, utils
+from .native import CallFlags
 
 
 class NEF(serialization.ISerializable):
-    def __init__(self, compiler_name: str = None, version: str = None, script: bytes = None):
+    def __init__(self,
+                 compiler_name: str = None,
+                 version: str = None,
+                 script: bytes = None,
+                 tokens: List[MethodToken] = None):
         """
         Create a Neo Executable Format file.
 
@@ -27,12 +33,16 @@ class NEF(serialization.ISerializable):
         self._checksum = 0
         # this is intentional, because NEO computes the initial checksum by serializing itself while checksum is 0
         self._checksum = self.compute_checksum()
+        self.tokens = [] if tokens is None else tokens
 
     def __len__(self):
         return (
             s.uint32  # magic
             + 32  # compiler
             + 32  # version
+            + 2  # reserved
+            + utils.get_var_size(self.tokens)
+            + 2  # reserved
             + s.uint32  # checksum
             + utils.get_var_size(self.script))
 
@@ -61,6 +71,9 @@ class NEF(serialization.ISerializable):
         writer.write_uint32(self.magic)
         writer.write_bytes(self.compiler.encode('utf-8'))
         writer.write_bytes(self.version.encode('utf-8'))
+        writer.write_bytes(b'\x00\x00')
+        writer.write_serializable_list(self.tokens)
+        writer.write_bytes(b'\x00\x00')
         writer.write_var_bytes(self.script)
         writer.write_uint32(self._checksum)
 
@@ -75,6 +88,12 @@ class NEF(serialization.ISerializable):
             raise ValueError("Deserialization error - Incorrect magic")
         self.compiler = reader.read_bytes(32).decode('utf-8')
         self.version = reader.read_bytes(32).decode('utf-8')
+        if reader.read_uint16() != 0:
+            raise ValueError("Reserved bytes must be 0")
+        self.tokens = reader.read_serializable_list(MethodToken)
+        if reader.read_uint16() != 0:
+            raise ValueError("Reserved bytes must be 0")
+
         self.script = reader.read_var_bytes(max=512 * 1024)
         if len(self.script) == 0:
             raise ValueError("Deserialization error - Script can't be empty")
@@ -96,3 +115,38 @@ class NEF(serialization.ISerializable):
         c = cls()
         c._checksum = 0
         return c
+
+
+class MethodToken(serialization.ISerializable):
+    def __init__(self,
+                 hash: types.UInt160,
+                 method: str,
+                 parameters_count: int,
+                 rvcount: int,
+                 call_flags: CallFlags):
+        self.hash = hash
+        self.method = method
+        self.parameters_count = parameters_count
+        self.rvcount = rvcount
+        self.call_flags = call_flags
+
+    def __len__(self):
+        return s.uint160 + utils.get_var_size(self.method) + s.uint16 + s.uint16 + s.uint8
+
+    def serialize(self, writer: serialization.BinaryWriter) -> None:
+        writer.write_serializable(self.hash)
+        writer.write_var_string(self.method)
+        writer.write_uint16(self.parameters_count)
+        writer.write_uint16(self.rvcount)
+        writer.write_uint8(self.call_flags.value)
+
+    def deserialize(self, reader: serialization.BinaryReader) -> None:
+        self.hash = reader.read_serializable(types.UInt160)
+        self.method = reader.read_var_string(32)
+        self.parameters_count = reader.read_uint16()
+        self.rvcount = reader.read_uint16()
+        self.call_flags = CallFlags(reader.read_uint8())
+
+    @classmethod
+    def _serializable_init(cls):
+        return cls(types.UInt160.zero(), "", 0, 0, CallFlags.NONE)
