aad6dfe92ab4cf74e113725f5228cbf1c5d1f27f
Lucas Kenji Uezu <49196318+luc10921@users.noreply.github.com>
2021-05-07 10:33:26 -0300

    Account creation using NEP-2 #60 (#81)
    * Account creation using NEP-2 #60
    
    * Finished testing.
    
    * Changed the constructor and moved all local imports to the top of the file
    
    * added some descriptions to methods
    
    * generated new accounts using rc1 instead of preview3
    
    * updated code according to feedback
    
    * added a test trying to create an account with an encrypted key and the wrong password
    
    * added validade_address() and removed the underscore after `data`.
    
    Co-authored-by: Erik van den Brink <git@erikvandenbrink.com>
    Co-authored-by: luc10921 <lucas.uezu@simpli.com.br>

---
+++ neo3/wallet/account.py
@@ -0,0 +1,297 @@
+from __future__ import annotations
+
+import base58
+import hashlib
+import unicodedata
+from typing import Optional
+
+from Crypto.Cipher import AES
+from neo3 import settings, contracts
+from neo3.core import types, to_script_hash
+from neo3.core.cryptography import ECPoint
+from neo3.core.cryptography import KeyPair
+
+# both constants below are used to encrypt/decrypt a private key to/from a nep2 key
+NEP_HEADER = bytes([0x01, 0x42])
+NEP_FLAG = bytes([0xe0])
+
+
+class Account:
+
+    def __init__(self, password: str,
+                 private_key: Optional[bytes] = None,
+                 watch_only: bool = False,
+                 address: Optional[str] = None
+                 ):
+        """
+        Instantiate an account. This constructor should only be directly called when it's desired to create a new
+        account using just a password and a randomly generated private key, otherwise, they should call another
+        function.
+        """
+
+        public_key: Optional[ECPoint] = None
+        encrypted_key: Optional[bytes] = None
+
+        if watch_only:
+            if address is None:
+                raise ValueError("Creating a watch only account requires an address")
+            else:
+                self.validate_address(address)
+
+        else:
+            key_pair: KeyPair
+
+            if private_key is None:
+                key_pair = KeyPair.generate()
+                private_key = key_pair.private_key
+            else:
+                key_pair = KeyPair(private_key)
+            encrypted_key = self.private_key_to_nep2(private_key, password)
+            script_hash = to_script_hash(contracts.Contract.create_signature_redeemscript(key_pair.public_key))
+            address = self.script_hash_to_address(script_hash)
+            public_key = key_pair.public_key
+
+        self.address: str = address
+        self.public_key = public_key
+        self.encrypted_key = encrypted_key
+
+    @property
+    def script_hash(self) -> types.UInt160:
+        return self.address_to_script_hash(self.address)
+
+    @classmethod
+    def from_encrypted_key(cls, nep2_key: str, password: str) -> Account:
+        """
+        Instantiate and returns an account from a given nep2 key and password.
+
+        Args:
+            nep2_key: the encrypted private key.
+            password: the password to decrypt the nep2 key.
+
+        Returns:
+            The newly created account.
+        """
+        return cls(password=password, private_key=cls.private_key_from_nep2(nep2_key, password))
+
+    @classmethod
+    def from_private_key(cls, private_key: bytes, password: str) -> Account:
+        """
+        Instantiate and returns an account from a given private key and password.
+
+        Args:
+            private_key: the private key that will be used to create an encrypted key.
+            password: the password to encrypt a randomly generated private key.
+
+        Returns:
+            The newly created account.
+        """
+        return cls(password=password, private_key=private_key)
+
+    @classmethod
+    def watch_only(cls, script_hash: types.UInt160) -> Account:
+        """
+        Instantiate and returns a watch-only account from a given script hash.
+
+        Args:
+            script_hash: the script hash that will identify an account to be watched.
+
+        Returns:
+            The account that will be monitored.
+        """
+        return cls(password='', watch_only=True, address=cls.script_hash_to_address(script_hash))
+
+    @classmethod
+    def watch_only_from_address(cls, address: str) -> Account:
+        """
+        Instantiate and returns a watch-only account from a given address.
+
+        Args:
+            address: the address that will identify an account to be watched.
+
+        Returns:
+            The account that will be monitored.
+        """
+        return cls(password='', watch_only=True, address=address)
+
+    @staticmethod
+    def script_hash_to_address(script_hash: types.UInt160) -> str:
+        """
+        Converts the specified script hash to an address.
+
+        Args:
+            script_hash: script hash to convert.
+        """
+        version = settings.network.account_version  # this is the current Neo's protocol version
+        data = version.to_bytes(1, 'little') + script_hash.to_array()
+
+        return base58.b58encode_check(data).decode('utf-8')
+
+    @staticmethod
+    def address_to_script_hash(address: str) -> types.UInt160:
+        """
+        Converts the specified address to a script hash.
+
+        Args:
+            address: address to convert
+
+        Raises:
+            ValueError: if the length of data (address value in bytes) is not valid.
+            ValueError: if the account version is not valid.
+        """
+        Account.validate_address(address)
+
+        data = base58.b58decode_check(address)
+
+        return types.UInt160(data[1:])
+
+    @staticmethod
+    def private_key_from_nep2(nep2_key: str, passphrase: str) -> bytes:
+        """
+        Decrypt a nep2 key into a private key.
+
+        Args:
+            nep2_key: the key that will be decrypt.
+            passphrase: the password to decrypt the nep2 key.
+
+        Raises:
+            ValueError: if the length of the nep2_key is not valid.
+            ValueError: if it's not possible to decode the nep2_key.
+            ValueError: if the passphrase is incorrect or the version of the account is not valid.
+
+        Returns:
+            the private key.
+        """
+        if len(nep2_key) != 58:
+            raise ValueError(f"Please provide a nep2_key with a length of 58 bytes (LEN: {len(nep2_key)})")
+
+        address_hash_size = 4
+        address_hash_offset = len(NEP_FLAG) + len(NEP_HEADER)
+
+        try:
+            decoded_key = base58.b58decode_check(nep2_key)
+        except Exception:
+            raise ValueError("Base58decode failure of nep2 key")
+
+        address_checksum = decoded_key[address_hash_offset:address_hash_offset + address_hash_size]
+        encrypted = decoded_key[-32:]
+
+        pwd_normalized = bytes(unicodedata.normalize("NFC", passphrase), "utf-8")
+        derived = hashlib.scrypt(password=pwd_normalized, salt=address_checksum,
+                                 n=16384,
+                                 r=8,
+                                 p=8,
+                                 dklen=64)
+
+        derived1 = derived[:32]
+        derived2 = derived[32:]
+
+        cipher = AES.new(derived2, AES.MODE_ECB)
+        decrypted = cipher.decrypt(encrypted)
+        private_key = Account._xor_bytes(decrypted, derived1)
+
+        # Now check that the address hashes match. If they don't, the password was wrong.
+        key_pair = KeyPair(private_key=private_key)
+        script_hash = to_script_hash(contracts.Contract.create_signature_redeemscript(key_pair.public_key))
+        address = Account.script_hash_to_address(script_hash)
+        first_hash = hashlib.sha256(address.encode("utf-8")).digest()
+        second_hash = hashlib.sha256(first_hash).digest()
+        checksum = second_hash[:4]
+        if checksum != address_checksum:
+            raise ValueError(f"Wrong passphrase or key was encrypted with an address version that is not"
+                             f"{settings.network.account_version}")
+
+        return private_key
+
+    @staticmethod
+    def private_key_to_nep2(private_key: bytes, passphrase: str) -> bytes:
+        """
+        Encrypt a private key into a nep2 key.
+
+        Args:
+            private_key: the key that will be encrypt.
+            passphrase: the password to encrypt the nep2 key.
+
+        Returns:
+            the encrypted nep2 key.
+        """
+        key_pair = KeyPair(private_key=private_key)
+        script_hash = to_script_hash(contracts.Contract.create_signature_redeemscript(key_pair.public_key))
+        address = Account.script_hash_to_address(script_hash)
+        # NEP2 checksum: hash the address twice and get the first 4 bytes
+        first_hash = hashlib.sha256(address.encode("utf-8")).digest()
+        second_hash = hashlib.sha256(first_hash).digest()
+        checksum = second_hash[:4]
+
+        pwd_normalized = bytes(unicodedata.normalize("NFC", passphrase), "utf-8")
+        derived = hashlib.scrypt(password=pwd_normalized, salt=checksum,
+                                 n=16384,
+                                 r=8,
+                                 p=8,
+                                 dklen=64)
+
+        derived1 = derived[:32]
+        derived2 = derived[32:]
+
+        xor_ed = Account._xor_bytes(bytes(private_key), derived1)
+        cipher = AES.new(derived2, AES.MODE_ECB)
+        encrypted = cipher.encrypt(xor_ed)
+
+        nep2 = bytearray()
+        nep2.extend(NEP_HEADER)
+        nep2.extend(NEP_FLAG)
+        nep2.extend(checksum)
+        nep2.extend(encrypted)
+
+        # Finally, encode with Base58Check
+        encoded_nep2 = base58.b58encode_check(bytes(nep2))
+
+        return encoded_nep2
+
+    @staticmethod
+    def _xor_bytes(a: bytes, b: bytes) -> bytes:
+        """
+        XOR on two bytes objects
+        Args:
+            a (bytes): object 1
+            b (bytes): object 2
+        Returns:
+            bytes: The XOR result
+        """
+        assert len(a) == len(b)
+        res = bytearray()
+        for i in range(len(a)):
+            res.append(a[i] ^ b[i])
+        return bytes(res)
+
+    @staticmethod
+    def is_valid_address(address: str) -> bool:
+        """
+        Test if the provided address is a valid address.
+
+        Args:
+            address: an address.
+        """
+        try:
+            Account.validate_address(address)
+        except ValueError:
+            return False
+        return True
+
+    @staticmethod
+    def validate_address(address: str):
+        """
+        Validate a given address. If address is not valid an exception will be raised.
+
+        Args:
+            address: an address.
+
+        Raises:
+            ValueError: if the length of data(address value in bytes) is not valid.
+            ValueError: if the account version is not valid.
+        """
+        data: bytes = base58.b58decode_check(address)
+        if len(data) != len(types.UInt160.zero()) + 1:
+            raise ValueError(f"The address is wrong, because data (address value in bytes) length should be "
+                             f"{len(types.UInt160.zero()) + 1}")
+        elif data[0] != settings.network.account_version:
+            raise ValueError(f"The account version is not {settings.network.account_version}")
---
+++ tests/wallet/test_account.py
@@ -0,0 +1,89 @@
+import unittest
+from neo3.wallet import Account
+
+# the accounts were generated using neo-cli v3.0.0-rc1
+account_list = [
+    {
+        "address": "NM4Hq7DDaMrNvhhN16aDiS3fLxXaqjGePy",
+        "encrypted_key": "6PYX6MzwNZHhxfUD82qTgATzUuGQgdajcnZQZuL5wKH77mo6JHLMB3o5zy",
+        "password": "city of zion",
+        "private_key": "58124574dfcca1a7a958775f6ea94e3d6c392ec3ba125b5bc591dd5e14f05e52",
+        "script_hash": "23ec53ddbbd356acd4a5e730aa7f5ec346048b0c",
+    },
+    {
+        "address": "NaeLNuvaTdB2pX3HjDrJdsjL2CxxkfLAKY",
+        "encrypted_key": "6PYWxh9YyxvjQUYVUkb8B6QbEAEFmF1RiNEm9D3aqfiSNHmW5SNidrJ3R9",
+        "password": "123",
+        "private_key": "2032b737522d22e2b6faf30555faa91d95c5aa5113c18f218f45815b6934c558",
+        "script_hash": "78f02e886e0e2cc8f3e679ca5da06868ffeb94a1",
+    },
+    {
+        "address": "NRn16PPhEKpgLRKg4C11ZQjfKSekwjuUkn",
+        "encrypted_key": "6PYUgEmLRdX4JkpBR6KLoQZZUKTSCBpp8VBNpGG148G1PuTqJyNsvXuUFh",
+        "password": "neo",
+        "private_key": "4c5182d9041f416bee1a6adac6a03f3e0319a83e75e78e6ff739304095791f19",
+        "script_hash": "6c70044e25ca0fcbe1d43f85e3cf8099ed044f40",
+    },
+    {
+        "address": "NUQnpUFs4Vw2nKyDtCK4tZNrdBUSYecXae",
+        "encrypted_key": "6PYKMoTLa16CkxX2q5RTfyJ17tVCY2QDYyfbeWoh7SFfzD7ReL1MChhijn",
+        "password": "neo-mamba",
+        "private_key": "1c43f87ce2ce3ea676bdfec4928705f3e9fedbdb3acf1fed6b3cc0c3d87c4cad",
+        "script_hash": "9f1992832dd88e3f8698e65058f645074d6a345d",
+    }
+]
+
+
+class AccountCreationTestCase(unittest.TestCase):
+
+    def test_new_account(self):
+        for testcase in account_list:
+            account = Account(testcase['password'])
+            self.assertIsNotNone(account)
+            self.assertIsNotNone(account.address)
+            self.assertIsNotNone(account.encrypted_key)
+            self.assertIsNotNone(account.public_key)
+
+    def test_new_account_from_private_key(self):
+        for testcase in account_list:
+            account = Account.from_private_key(bytes.fromhex(testcase['private_key']), testcase['password'])
+            self.assertEqual(testcase['address'], account.address)
+            self.assertEqual(testcase['encrypted_key'].encode('utf-8'), account.encrypted_key)
+            self.assertEqual(testcase['script_hash'], str(account.script_hash))
+            self.assertIsNotNone(account.public_key)
+
+    def test_new_account_from_encrypted_key(self):
+        for testcase in account_list:
+            account = Account.from_encrypted_key(testcase['encrypted_key'], testcase['password'])
+            self.assertEqual(testcase['address'], account.address)
+            self.assertEqual(testcase['encrypted_key'].encode('utf-8'), account.encrypted_key)
+            self.assertEqual(testcase['script_hash'], str(account.script_hash))
+            self.assertIsNotNone(account.public_key)
+
+    def test_new_watch_only_account(self):
+        from neo3.core.types import UInt160
+        for testcase in account_list:
+            account = Account.watch_only(UInt160.from_string(testcase['script_hash']))
+            self.assertEqual(testcase['address'], account.address)
+            self.assertIsNone(account.encrypted_key)
+            self.assertEqual(testcase['script_hash'], str(account.script_hash))
+            self.assertIsNone(account.public_key)
+
+    def test_new_watch_only_account_from_address(self):
+        for testcase in account_list:
+            account = Account.watch_only_from_address(testcase['address'])
+            self.assertEqual(testcase['address'], account.address)
+            self.assertIsNone(account.encrypted_key)
+            self.assertEqual(testcase['script_hash'], str(account.script_hash))
+            self.assertIsNone(account.public_key)
+
+    def test_new_account_wrong_password(self):
+        wrong_password: bool = True
+        for testcase in account_list:
+            try:
+                Account.from_encrypted_key(testcase['encrypted_key'], "wrong password")
+                wrong_password = False
+            except ValueError:
+                continue
+
+        self.assertEqual(True, wrong_password)
