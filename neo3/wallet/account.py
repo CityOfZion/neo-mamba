ab1c55989ec7c61c699706dd65b995f027ad01ab
Erik van den Brink <git@erikvandenbrink.com>
2021-05-28 09:42:28 +0200

    merge account and accountcontract into one module

--- neo3/wallet/account.py
+++ neo3/wallet/account.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 import base58  # type: ignore
+import base64
 import hashlib
 import unicodedata
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 from Crypto.Cipher import AES
 from jsonschema import validate  # type: ignore
 from neo3 import settings, contracts
 from neo3.core import types, to_script_hash
 from neo3.core.cryptography import ECPoint, KeyPair
-from neo3.wallet.accountcontract import AccountContract
 from neo3.wallet.scrypt_parameters import ScryptParameters
 
 # both constants below are used to encrypt/decrypt a private key to/from a nep2 key
@@ -20,8 +20,76 @@ WIF_SUFFIX = bytes([0x01])
 PRIVATE_KEY_LENGTH = 32
 
 
-class Account:
+class AccountContract(contracts.Contract):
+    _contract_params_schema = {
+        "type": ["object", "null"],
+        "properties": {
+            "name": {"type": "string"},
+            "type": {"type": "string"}
+        },
+        "required": ["name", "type"]
+    }
+    _json_schema = {
+        "type": ["object", "null"],
+        "properties": {
+            "script": {"type": "string"},
+            "parameters": {
+                "type": "array",
+                "items": _contract_params_schema,
+                "minItems": 0,
+            },
+            "deployed": {"type": "boolean"}
+        },
+        "required": ["script", "parameters", "deployed"]
+    }
 
+    def __init__(self, script: bytes, parameter_list: List[contracts.ContractParameterDefinition]):
+        super().__init__(script, [param.type for param in parameter_list])
+
+        self.parameter_names: List[str] = [param.name for param in parameter_list]
+        self.deployed: bool = False
+
+    @classmethod
+    def from_contract(cls, contract: contracts.Contract) -> AccountContract:
+        if isinstance(contract, AccountContract):
+            return contract
+
+        parameters = [contracts.ContractParameterDefinition(f"arg{index}", contract.parameter_list[index])
+                      for index in range(len(contract.parameter_list))]
+        return cls(script=contract.script,
+                   parameter_list=parameters)
+
+    @classmethod
+    def from_json(cls, json: dict) -> AccountContract:
+        """
+        Parse object out of JSON data.
+
+        Args:
+            json: a dictionary.
+        """
+        validate(json, schema=cls._json_schema)
+
+        contract = cls(
+            script=base64.b64decode(json['script']),
+            parameter_list=list(map(lambda p: contracts.ContractParameterDefinition.from_json(p), json['parameters']))
+        )
+        contract.deployed = json['deployed']
+
+        return contract
+
+    def to_json(self) -> dict:
+        """ Convert object into JSON representation. """
+        return {
+            'script': base64.b64encode(self.script).decode('utf-8'),
+            'parameters': list(map(lambda index: {'name': self.parameter_names[index],
+                                                  'type': self.parameter_list[index].PascalCase()
+                                                  },
+                                   range(len(self.parameter_list)))),
+            'deployed': self.deployed
+        }
+
+
+class Account:
     _json_schema = {
         "type": "object",
         "properties": {
