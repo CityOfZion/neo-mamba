3d280737d314ac198349e7d15e0538572f9913ea
Erik van den Brink <git@erikvandenbrink.com>
2022-10-24 09:34:25 +0200

    new examples

---
+++ examples/nep17-airdrop.py
@@ -0,0 +1,46 @@
+import asyncio
+from neo3.api.wrappers import Config, ChainFacade, NeoToken, NEP17Contract
+from neo3.api.helpers.signing import sign_insecure_with_account
+from neo3.wallet.wallet import Wallet
+from neo3.network.payloads.verification import Signer
+from neo3.core import types
+from examples import shared
+
+
+async def example_airdrop(neoxp: shared.NeoExpress):
+    # This example shows how to airdrop NEP-17 tokens
+    wallet = shared.user_wallet
+    account = wallet.account_default
+
+    config = Config(rpc_host=neoxp.rpc_host)
+    config.add_signer(
+        sign_insecure_with_account(account, pw="123"),
+        Signer(account.script_hash),  # default scope is CALLED_BY_ENTRY
+    )
+    facade = ChainFacade(config)
+
+    # Use the generic NEP17 class to wrap the token
+    token = NEP17Contract(shared.coz_token_hash)
+    balance = await facade.test_invoke(token.balance_of(account.address))
+    print(f"Current COZ token balance: {balance}")
+
+    # First we have to mint the tokens to our own wallet
+    # We do this by sending NEO to the contract
+    neo = NeoToken()
+    print("Minting..", end='')
+    print(await facade.invoke(neo.transfer(source=account.address, destination=shared.coz_token_hash, amount=100)))
+    print(await facade.invoke(neo.transfer(source=account.address, destination=shared.coz_token_hash, amount=100)))
+
+    balance = await facade.test_invoke(token.balance_of(account.address))
+    print(f"New COZ token balance: {balance}")
+
+    # Now let's airdrop the tokens
+    destination_addresses = ["NWuHQdxabXPdC6vVwJhxjYELDQPqc1d4TG", "NhVnpBxSRjkScZKHGzsEreYAMS1qRrNdaH", "NanYZRm6m6sa6Z6F3RBRYSXqdpg5rZqxdZ","NUqLhf1p1vQyP2KJjMcEwmdEBPnbCGouVp","NKuyBkoGdZZSLyPbJEetheRhMjeznFZszf"]
+    print("Airdropping 10 tokens and waiting for receipt")
+    print(await facade.invoke(token.transfer_multi(account.address, destination_addresses, 10)))
+
+
+if __name__ == "__main__":
+    with shared.NeoExpress(shared.neoxpress_config_path, shared.neoxpress_batch_path) as neoxp:
+        asyncio.run(example_airdrop(neoxp))
+    # asyncio.run(example_airdrop(shared.NeoExpress(shared.neoxpress_config_path)))
---
+++ examples/shared/__init__.py
@@ -0,0 +1,94 @@
+import pathlib
+import os
+import sys
+import shutil
+import subprocess
+import threading
+import time
+import json
+from typing import Optional
+from neo3.wallet.wallet import Wallet
+from neo3.core import types
+
+_shared_dir = pathlib.Path("shared").resolve(strict=True)
+
+user_wallet = Wallet.from_file(f"{_shared_dir}/user-wallet.json", password="123")
+coz_wallet = Wallet.from_file(f"{_shared_dir}/coz-wallet.json", password="123")
+neoxpress_config_path = f"{_shared_dir}/default.neo-express"
+neoxpress_batch_path = f"{_shared_dir}/setup-neoxp-for-tests.batch"
+coz_token_hash = types.UInt160.from_string("0xb31569ff17cde6a3935592608b606ac3cdd591df")
+
+
+class NeoExpress:
+    """Neo express wrapper"""
+
+    def __init__(self,
+                 config_path: str,
+                 batch_path: Optional[str] = None,
+                 executable_path: Optional[str] = None,
+                 return_delay: int = 1,
+                 debug:bool = False):
+        self.prog = "neoxp"
+        self.config_path = config_path
+        self.batch_path = batch_path
+        self.return_delay = return_delay
+        self.debug = debug
+
+        with open(self.config_path) as f:
+            data = json.load(f)
+            port = data["consensus-nodes"][0]["rpc-port"]
+            address = data["settings"].get("rpc.BindAddress", "127.0.0.1")
+            self.rpc_host = f"http://{address}:{port}"
+
+        if executable_path is not None:
+            self._verify_executable(executable_path)
+            self.prog = executable_path
+        else:
+            if sys.platform == 'darwin':
+                try:
+                    subprocess.run(["bash", "-c", "neoxp -h"], check=True, stdout=subprocess.DEVNULL)
+                except subprocess.SubprocessError:
+                    raise ValueError("Cannot automatically find global neo express executable. Please specify the path")
+            elif shutil.which(self.prog) is None:
+                raise ValueError("Cannot automatically find global neo express executable. Please specify the path")
+
+    def _verify_executable(self, full_path: str):
+        if not os.path.isfile(full_path) or not os.access(full_path, os.X_OK):
+            raise ValueError(f"Invalid executable: {full_path}")
+
+    def initialize_with(self, batch_path: str):
+        cmd = f"neoxp batch -r {batch_path}"
+        if sys.platform == 'darwin':
+            subprocess.run(["bash", "-c", cmd], check=True, stdout=subprocess.DEVNULL)
+        else:
+            subprocess.run(cmd.split(" "), check=True, stdout=subprocess.DEVNULL)
+
+    def run(self, return_delay=None):
+        cmd = f"neoxp run -i {self.config_path}"
+        kwargs = {"check": True}
+        if self.debug is False:
+            kwargs["stdout"] = subprocess.DEVNULL
+
+        if sys.platform == 'darwin':
+            kwargs["args"] = ["bash", "-c", cmd]
+        else:
+            kwargs["args"] = cmd.split(" ")
+        thread = threading.Thread(target=subprocess.run, kwargs=kwargs, name="neoxp", daemon=True)
+        thread.start()
+        time.sleep(return_delay if return_delay else self.return_delay)
+
+    def stop(self):
+        cmd = f"neoxp stop -a -i {self.config_path}"
+        if sys.platform == 'darwin':
+            subprocess.run(["bash", "-c", cmd], check=True, stdout=subprocess.DEVNULL)
+        else:
+            subprocess.run(cmd.split(" "), check=True, stdout=subprocess.DEVNULL)
+
+    def __enter__(self):
+        if self.batch_path is not None:
+            self.initialize_with(self.batch_path)
+        self.run(self.return_delay)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
