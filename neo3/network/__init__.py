941126f305f2acf890803b4c76d10aa8fbd289a2
Erik van den Brink <git@erikvandenbrink.com>
2023-12-13 11:14:29 +0100

    fix aiodns for Windows

--- neo3/network/__init__.py
+++ neo3/network/__init__.py
@@ -2,3 +2,8 @@
 P2P network classes. Holds `Block` and `Transaction` payloads (among others), a network node and helper classes for
  syncing the chain.
 """
+import sys
+import asyncio
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
