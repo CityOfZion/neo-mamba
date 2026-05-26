5f73204f942278187b7b8cfa3d23c332300afb17
Erik van den Brink <git@erikvandenbrink.com>
2026-05-26 15:08:59 +0200

    compiler: add support for various `System.Runtime` calls

--- neo3/sc/runtime/__init__.py
+++ neo3/sc/runtime/__init__.py
@@ -5,11 +5,17 @@ from neo3.sc.compiletime import syscall
 
 @syscall("System.Runtime.GetScriptContainer")
 def get_script_container() -> TrimmedTransaction:
+    """
+    Gets the current script container. Which in a smart contract is always the calling transaction.
+    """
     pass
 
 
 @syscall("System.Runtime.GetCallingScriptHash")
 def get_calling_script_hash() -> UInt160:  # TODO: these should be optional
+    """
+    Gets the script hash of the calling contract.
+    """
     pass
 
 
@@ -17,6 +23,9 @@ def get_calling_script_hash() -> UInt160:  # TODO: these should be optional
 def get_entry_script_hash() -> (
     UInt160
 ):  # TODO: these should be optional but can they actually ever be in a smart contract?
+    """
+    Gets the script hash of the entry context
+    """
     pass
 
 
@@ -30,9 +39,53 @@ def get_executing_script_hash() -> UInt160:
 
 @syscall("System.Runtime.GetRandom")
 def get_random() -> int:
+    """
+    Gets the random number generated from the VRF
+    """
     pass
 
 
 @syscall("System.Runtime.CheckWitness")
 def check_witness(hash: UInt160 | ECPoint) -> bool:
+    """
+    Determines whether the specified account has witnessed the current transaction
+
+    Args:
+        hash: the hash or public key of the account to verify against.
+    """
+    pass
+
+
+@syscall("System.Runtime.BurnGas")
+def burn_gas(datoshi: int) -> None:
+    """
+    Burning GAS to benefit the NEO ecosystem.
+
+    Args:
+        datoshi: the amount of GAS to burn, in the unit of datoshi, 1 datoshi = 1e-8 GAS
+    """
+    pass
+
+
+@syscall("System.Runtime.GasLeft")
+def gas_left() -> int:
+    """
+    Gets the remaining GAS that can be spent in order to complete the execution
+    """
+    pass
+
+
+@syscall("System.Runtime.GetTime")
+def get_time() -> int:
+    """
+    Gets the timestamp of the current block
+    """
+    pass
+
+
+@syscall("System.Runtime.GetInvocationCounter")
+def get_invocation_counter() -> int:
+    """
+    Gets the number of times the current contract has been called during the execution
+    """
     pass
