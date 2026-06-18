7182916c5c7042817bb6da7f15494a8669944fe3
Erik van den Brink <git@erikvandenbrink.com>
2026-06-18 09:11:54 +0200

    touchup

--- cli/cmd_disassemble.py
+++ cli/cmd_disassemble.py
@@ -2,14 +2,13 @@ import sys
 import base64
 import argparse
 from neo3.compiler.disassembler import disassemble
+from neo3.contracts.nef import NEF
 
 
 def disassemble_bytecode(args: argparse.Namespace) -> int:
     try:
         if args.nef:
-            from neo3.contracts.nef import NEF
-            nef = NEF.from_file(args.input)
-            bytecode = nef.script
+            bytecode = NEF.from_file(args.input).script
         elif args.base64:
             bytecode = base64.b64decode(args.input, validate=True)
         else:
