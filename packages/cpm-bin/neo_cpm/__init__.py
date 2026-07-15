af6f4db606fd13d1d83120ecf30d8d3d46db3315
ixje <git@erikvandenbrink.com>
2026-07-15 14:22:05 +0200

    cpm-bin: add package (#407)

---
+++ packages/cpm-bin/neo_cpm/__init__.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import stat
+import platform
+
+
+def get_binary_path() -> Path:
+    suffix = ".exe" if platform.system() == "Windows" else ""
+    path = Path(__file__).parent / "bin" / f"cpm{suffix}"
+    path.chmod(path.stat().st_mode | stat.S_IEXEC)
+    return path
